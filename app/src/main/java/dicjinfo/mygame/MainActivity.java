bcc29d18f7b2121c18261ce18b48b4499e3c231e
Carl Gauthier <gauthier_carl004@hotmail.com>
2017-01-17 13:25:15 -0500

    Initial commit

---
+++ app/src/main/java/dicjinfo/mygame/MainActivity.java
@@ -0,0 +1,53 @@
+package dicjinfo.mygame;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor gyro;
+    float z;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fullscreenInit();
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        gyro = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        sensorManager.registerListener(this, gyro, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+            z += event.values[2];
+
+            String str = Float.toString(z);
+            TextView txtv = (TextView)findViewById(R.id.txt);
+            txtv.setText(str);
+        }
+    }
+
+    private void fullscreenInit() {
+
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
